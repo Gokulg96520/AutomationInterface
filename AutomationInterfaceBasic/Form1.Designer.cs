6f36cf6fa542bdbd89c1bd6ee052cb90cd3ffed3
GokulG <gokulg96520@gmail.com>
2025-02-28 16:04:33 +0530

    Added Basic TwinCAT Project & PLC Project Creation

--- AutomationInterfaceBasic/Form1.Designer.cs
+++ AutomationInterfaceBasic/Form1.Designer.cs
@@ -30,6 +30,10 @@ namespace AutomationInterfaceBasic
         private void InitializeComponent()
         {
             this.btnCreateTwinCATInstance = new System.Windows.Forms.Button();
+            this.btnCreateTwinCATPrj = new System.Windows.Forms.Button();
+            this.btnActivateConfig = new System.Windows.Forms.Button();
+            this.btnRestartTwincat = new System.Windows.Forms.Button();
+            this.btnCreatePLCProject = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateTwinCATInstance
@@ -42,11 +46,55 @@ namespace AutomationInterfaceBasic
             this.btnCreateTwinCATInstance.UseVisualStyleBackColor = true;
             this.btnCreateTwinCATInstance.Click += new System.EventHandler(this.btnCreateTwinCATInstance_Click);
             // 
+            // btnCreateTwinCATPrj
+            // 
+            this.btnCreateTwinCATPrj.Location = new System.Drawing.Point(12, 51);
+            this.btnCreateTwinCATPrj.Name = "btnCreateTwinCATPrj";
+            this.btnCreateTwinCATPrj.Size = new System.Drawing.Size(141, 23);
+            this.btnCreateTwinCATPrj.TabIndex = 1;
+            this.btnCreateTwinCATPrj.Text = "Create TwinCAT Project";
+            this.btnCreateTwinCATPrj.UseVisualStyleBackColor = true;
+            this.btnCreateTwinCATPrj.Click += new System.EventHandler(this.btnCreateTwinCATPrj_Click);
+            // 
+            // btnActivateConfig
+            // 
+            this.btnActivateConfig.Location = new System.Drawing.Point(159, 51);
+            this.btnActivateConfig.Name = "btnActivateConfig";
+            this.btnActivateConfig.Size = new System.Drawing.Size(125, 23);
+            this.btnActivateConfig.TabIndex = 2;
+            this.btnActivateConfig.Text = "Activate Configuration";
+            this.btnActivateConfig.UseVisualStyleBackColor = true;
+            this.btnActivateConfig.Click += new System.EventHandler(this.btnActivateConfig_Click);
+            // 
+            // btnRestartTwincat
+            // 
+            this.btnRestartTwincat.Location = new System.Drawing.Point(290, 51);
+            this.btnRestartTwincat.Name = "btnRestartTwincat";
+            this.btnRestartTwincat.Size = new System.Drawing.Size(109, 23);
+            this.btnRestartTwincat.TabIndex = 3;
+            this.btnRestartTwincat.Text = "Restart TwinCAT";
+            this.btnRestartTwincat.UseVisualStyleBackColor = true;
+            this.btnRestartTwincat.Click += new System.EventHandler(this.btnRestartTwincat_Click);
+            // 
+            // btnCreatePLCProject
+            // 
+            this.btnCreatePLCProject.Location = new System.Drawing.Point(12, 90);
+            this.btnCreatePLCProject.Name = "btnCreatePLCProject";
+            this.btnCreatePLCProject.Size = new System.Drawing.Size(141, 23);
+            this.btnCreatePLCProject.TabIndex = 4;
+            this.btnCreatePLCProject.Text = "Create PLC Project";
+            this.btnCreatePLCProject.UseVisualStyleBackColor = true;
+            this.btnCreatePLCProject.Click += new System.EventHandler(this.btnCreatePLCProject_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreatePLCProject);
+            this.Controls.Add(this.btnRestartTwincat);
+            this.Controls.Add(this.btnActivateConfig);
+            this.Controls.Add(this.btnCreateTwinCATPrj);
             this.Controls.Add(this.btnCreateTwinCATInstance);
             this.Name = "Main";
             this.Text = "Main";
@@ -57,6 +105,10 @@ namespace AutomationInterfaceBasic
         #endregion
 
         private System.Windows.Forms.Button btnCreateTwinCATInstance;
+        private System.Windows.Forms.Button btnCreateTwinCATPrj;
+        private System.Windows.Forms.Button btnActivateConfig;
+        private System.Windows.Forms.Button btnRestartTwincat;
+        private System.Windows.Forms.Button btnCreatePLCProject;
     }
 }
 
--- AutomationInterfaceBasic/Form1.cs
+++ AutomationInterfaceBasic/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TCatSysManagerLib;
 
 namespace AutomationInterfaceBasic
 {
@@ -14,6 +15,11 @@ namespace AutomationInterfaceBasic
     {
         private Type t;
         private EnvDTE.DTE dte;
+        private EnvDTE.Solution sol;
+        private EnvDTE.Project prj;
+        private ITcSysManager sysManager;
+        private ITcSmTreeItem plc;
+        private ITcSmTreeItem newPlcProject;
 
         public Main()
         {
@@ -29,7 +35,60 @@ namespace AutomationInterfaceBasic
                 dte.SuppressUI = false;
                 dte.MainWindow.Visible = true;
             });
-           
         }
+
+        private async void btnCreateTwinCATPrj_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                sol = dte.Solution;
+                sol.Create(@"C:\Users\GokulG\Desktop\New folder", "SolProj");
+                sol.SaveAs(@"C:\Users\GokulG\Desktop\New folder\SolProj.sln");
+                string template = @"C:\TwinCAT\3.1\Components\Base\PrjTemplate\TwinCAT Project.tsproj"; //path to project template
+                prj = sol.AddFromTemplate(template, @"C:\Users\GokulG\Desktop\New folder\TwinCATPrj", "TwinCATPrj");
+                
+            });
+        }
+
+        private async void btnActivateConfig_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                if (sysManager == null)
+                {
+                    sysManager = (ITcSysManager)prj.Object;
+                }
+                sysManager.ActivateConfiguration();
+            });
+        }
+
+        private async void btnRestartTwincat_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                if (sysManager == null)
+                {
+                    sysManager = (ITcSysManager)prj.Object;
+                }
+                sysManager.StartRestartTwinCAT();
+            });
+        }
+
+        private async void btnCreatePLCProject_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                if (sysManager == null){
+                    sysManager = (ITcSysManager)prj.Object;
+                }
+                plc = sysManager.LookupTreeItem("TIPC");
+                //string pathToTemplateFile = @"C:\TwinCAT\3.x\Components\Plc\PlcTemplate\Plc Templates\Standard PLC Template.plcproj";
+                //ITcSmTreeItem newProject = plc.CreateChild("NameOfProject", 0, "", pathToTemplateFile);
+                //If you are using beckhoff template just enter the template name path is not Required;
+                newPlcProject = plc.CreateChild("PLCProject1", 0, "", "Standard PLC Template.plcproj"); 
+            });
+        }
+
+
     }
 }
